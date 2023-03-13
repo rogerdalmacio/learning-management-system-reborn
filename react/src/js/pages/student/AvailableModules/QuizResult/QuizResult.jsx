e8b2928e5d24a39d806c2bcbac80bdd68982733b
Victor Higoy <higoyvictor143@gmail.com>
2023-03-13 22:03:20 +0800

    remove non usable codes in quizresult

--- react/src/js/pages/student/AvailableModules/QuizResult/QuizResult.jsx
+++ react/src/js/pages/student/AvailableModules/QuizResult/QuizResult.jsx
@@ -50,46 +50,6 @@ const QuizResult = ({
           />
         </div>
       );
-    } else if (percentageExam) {
-      return (
-        <div className="quizResultContainer">
-          <CircularProgressbar
-            value={percentageExam}
-            text={`${percentageExam}%`}
-            maxValue={100}
-            styles={buildStyles({
-              // Can specify path transition in more detail, or remove it entirely
-              // pathTransition: 'none',
-              textSize: "25px",
-              // Colors
-              pathColor: `rgba(7, 23, 122, 1)`,
-              textColor: "#252525",
-              // trailColor: "#1b84d6",
-              backgroundColor: "#07177a",
-            })}
-          />
-        </div>
-      );
-    } else if (percentageExam2) {
-      return (
-        <div className="quizResultContainer">
-          <CircularProgressbar
-            value={percentageExam2}
-            text={`${percentageExam2}`}
-            maxValue={50}
-            styles={buildStyles({
-              // Can specify path transition in more detail, or remove it entirely
-              // pathTransition: 'none',
-              textSize: "25px",
-              // Colors
-              pathColor: `rgba(7, 23, 122, 1)`,
-              textColor: "#252525",
-              // trailColor: "#1b84d6",
-              backgroundColor: "#07177a",
-            })}
-          />
-        </div>
-      );
     }
   };
 
